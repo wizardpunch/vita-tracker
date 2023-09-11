47802012e39f31ab57e06ed9f174c43b21a8bab2
Shun <miyakeshun@gmail.com>
2023-09-11 01:12:23 -0700

    Implemented Frontend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -16,5 +16,9 @@ router.route('/add').post((req, res) => {
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
-
+router.route('/:id').delete((req, res) => {
+  Name.findByIdAndDelete(req.params.id)
+    .then(() => res.json('User deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 module.exports = router;
